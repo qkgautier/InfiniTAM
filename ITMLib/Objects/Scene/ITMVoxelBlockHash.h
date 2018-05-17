f3d3fba20e75b0aa1318db3829420d02aec5970e
Quentin Gautier <qkgautier@eng.ucsd.edu>
2018-05-16 17:35:38 -0700

    Adjusted some constants and commented out stuff

--- ITMLib/Objects/Scene/ITMVoxelBlockHash.h
+++ ITMLib/Objects/Scene/ITMVoxelBlockHash.h
@@ -23,11 +23,11 @@
 //#define SDF_EXCESS_LIST_SIZE 0x30000     // Size of excess list, used to handle collisions. Also max offset (unsigned short) value.
 
 // for loop closure
-#define SDF_LOCAL_BLOCK_NUM 0x10000		// Number of locally stored blocks, currently 2^12
+#define SDF_LOCAL_BLOCK_NUM 0x8000		// Number of locally stored blocks, currently 2^12
 
-#define SDF_BUCKET_NUM 0x40000			// Number of Hash Bucket, should be 2^n and bigger than SDF_LOCAL_BLOCK_NUM, SDF_HASH_MASK = SDF_BUCKET_NUM - 1
-#define SDF_HASH_MASK 0x3ffff			// Used for get hashing value of the bucket index,  SDF_HASH_MASK = SDF_BUCKET_NUM - 1
-#define SDF_EXCESS_LIST_SIZE 0x8000		// 0x8000 Size of excess list, used to handle collisions. Also max offset (unsigned short) value.
+#define SDF_BUCKET_NUM 0x8000			// Number of Hash Bucket, should be 2^n and bigger than SDF_LOCAL_BLOCK_NUM, SDF_HASH_MASK = SDF_BUCKET_NUM - 1
+#define SDF_HASH_MASK (SDF_BUCKET_NUM-1)			// Used for get hashing value of the bucket index,  SDF_HASH_MASK = SDF_BUCKET_NUM - 1
+#define SDF_EXCESS_LIST_SIZE 0x6000		// 0x8000 Size of excess list, used to handle collisions. Also max offset (unsigned short) value.
 
 
 
